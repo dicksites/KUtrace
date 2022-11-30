cd5b6f975dfb749ce9aa32c7e2ff0dba351efc89
Dick Sites <dick.sites@gmail.com>
2022-11-30 10:09:17 -0800

    Strip off any .trace in filename

--- postproc/postproc3.sh
+++ postproc/postproc3.sh
@@ -1,17 +1,15 @@
-#!/bin/sh
-#
-# Postprocess a raw KUtrace file into a dynamic HTML file and open it in chrome
-#
-# arg 1 filename stem (no .trace), arg 2 "title", arg 3/4 spantotrim args
-#
+#!/bin/bash
+# arg 1 filename stem (no .trace), arg 2 "title", arg 3/4 spantrim args
 
-# Must sort by byte code, not local collating sequence
+# Must sort by pure byte values, not local collating sequence
 export LC_ALL=C
 
-cat $1.trace  |./rawtoevent |sort -n |./eventtospan3 "$2" |sort >$1.json 
-echo "  $1.json written"
+# Strip trailing .trace if it is there
+var1=${1%.trace}
 
-# Default to no trim, else start/stop seconds
+cat $var1.trace  |./rawtoevent |sort -n |./eventtospan3 "$2" |sort >$var1.json 
+echo "  $var1.json written"
+
 trim_arg='0'
 if [ -n "$3" ]
 then
@@ -19,8 +17,7 @@ delimit=' '
 trim_arg=$3$delimit$4
 fi
 
-cat $1.json |./spantotrim $trim_arg |./makeself show_cpu.html >$1.html
-echo "  $1.html written"
+cat $var1.json |./spantotrim $trim_arg |./makeself show_cpu.html >$var1.html
+echo "  $var1.html written"
 
-# Open up in Chrome
-google-chrome $1.html &
+google-chrome $var1.html &
