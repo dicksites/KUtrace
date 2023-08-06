3d3ae46491f279d7dfb7c171bae7b36844152c0e
Dick Sites <dick.sites@gmail.com>
2023-08-06 12:07:07 -0700

    Riscv uses GNU2 license

--- riscv_linux/loadable-module/kutrace_mod.c
+++ riscv_linux/loadable-module/kutrace_mod.c
@@ -3,6 +3,8 @@
  *
  * Author: Richard Sites <dick.sites@gmail.com>
  *
+ * SPDX-License-Identifier: GPL-2.0
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
